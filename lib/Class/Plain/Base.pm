6205d1b27e473f951f631df0bd6c67e139bc3636
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-09-21 13:06:44 +0900

    Support hash reference arg of new

--- lib/Class/Plain/Base.pm
+++ lib/Class/Plain/Base.pm
@@ -4,11 +4,11 @@ use strict;
 use warnings;
 
 sub new {
- my $class = shift;
- 
- my $self = {@_};
- 
- return bless $self, ref $class || $class;
+  my $class = shift;
+  
+  my $self = ref $_[0] ? {%$_[0]} : {@_};
+  
+  bless $self, ref $class || $class;
 }
 
 1;
