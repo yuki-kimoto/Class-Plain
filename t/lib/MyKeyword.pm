0cb26a3080e25505cf95836e652f8efa09405a8f
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-09-22 07:45:54 +0900

    Add keyword tests

--- t/lib/MyKeyword.pm
+++ t/lib/MyKeyword.pm
@@ -3,23 +3,29 @@ package MyKeyword;
 use strict;
 use warnings;
 
-sub class {
-  
-}
+sub class {}
 
-sub field {
-  
-}
+sub field {}
 
-sub method {
-  
-}
+sub method {}
+
+sub isa {}
+
+sub reader {}
+
+sub common {}
 
 class(1);
 
 field(1);
 
 method(3);
+
+isa(4);
+
+reader(5);
+
+common(6);
 
 1;
 
